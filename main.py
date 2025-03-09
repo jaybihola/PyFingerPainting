b5fe008de4b46b75ea0811e4cf4305e9aa98d58a
Jay Bihola <jaybih@outlook.com>
2025-03-08 21:09:49 -0500

    add constants file

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import mediapipe as mp
 import cv2
 from pynput import keyboard
+import constants
 import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-VIEW_MODE = 0
-MODE_SWITCHER = 1
-FREE_DRAW_MODE = 2
-SHAPE_MODE = 3
-COLOR_MODE = 4
-
-RED = (0, 0, 255)
-GREEN = (0, 255, 0)
-BLUE = (255, 0, 0)
-YELLOW = (0, 255, 255)
-PURPLE = (255, 0, 255)
-
-current_color = BLUE
-current_mode = FREE_DRAW_MODE
+current_color = constants.BLUE
+current_mode = constants.FREE_DRAW_MODE
 prev_mode = None
 
 hands = mp_hands.Hands(
@@ -39,50 +28,50 @@ def on_press(key):
     global points
     global prev_mode
 
-    if current_mode == MODE_SWITCHER:
+    if current_mode == constants.MODE_SWITCHER:
         if key == keyboard.KeyCode.from_char('f'):
-            current_mode = FREE_DRAW_MODE
+            current_mode = constants.FREE_DRAW_MODE
         if key == keyboard.KeyCode.from_char('s'):
-            current_mode = SHAPE_MODE
+            current_mode = constants.SHAPE_MODE
         if key == keyboard.KeyCode.from_char('v'):
-            current_mode = VIEW_MODE
+            current_mode = constants.VIEW_MODE
 
     if key == keyboard.Key.space:
-        current_mode = MODE_SWITCHER
+        current_mode = constants.MODE_SWITCHER
 
-    if current_mode == FREE_DRAW_MODE or current_mode == SHAPE_MODE:
+    if current_mode == constants.FREE_DRAW_MODE or current_mode == constants.SHAPE_MODE:
         if key == keyboard.KeyCode.from_char('c'):
             prev_mode = current_mode
-            current_mode = COLOR_MODE
+            current_mode = constants.COLOR_MODE
 
-    if current_mode == COLOR_MODE:
+    if current_mode == constants.COLOR_MODE:
         if key == keyboard.KeyCode.from_char('r'):
             print ('Color changed to RED')
-            current_color = RED
+            current_color = constants.RED
             if prev_mode:
                 current_mode = prev_mode
                 prev_mode = None
         if key == keyboard.KeyCode.from_char('b'):
             print ('Color changed to BLUE')
-            current_color = BLUE
+            current_color = constants.BLUE
             if prev_mode:
                 current_mode = prev_mode
                 prev_mode = None
         if key == keyboard.KeyCode.from_char('g'):
             print ('Color changed to GREEN')
-            current_color = GREEN
+            current_color = constants.GREEN
             if prev_mode:
                 current_mode = prev_mode
                 prev_mode = None
         if key == keyboard.KeyCode.from_char('y'):
             print ('Color changed to YELLOW')
-            current_color = YELLOW
+            current_color = constants.YELLOW
             if prev_mode:
                 current_mode = prev_mode
                 prev_mode = None
         if key == keyboard.KeyCode.from_char('p'):
             print ('Color changed to PURPLE')
-            current_color = PURPLE
+            current_color = constants.PURPLE
             if prev_mode:
                 current_mode = prev_mode
                 prev_mode = None
@@ -118,36 +107,36 @@ while True:
 
             mp_drawing.draw_landmarks(image, main_hand, mp_hands.HAND_CONNECTIONS)
 
-            if current_mode == FREE_DRAW_MODE and i == 0:
+            if current_mode == constants.FREE_DRAW_MODE and i == 0:
                 points.append((x, y))
 
     for coord in points:
         cv2.circle(image, coord, 1, current_color, 12)
 
-    if current_mode == FREE_DRAW_MODE:
+    if current_mode == constants.FREE_DRAW_MODE:
         cv2.putText(image, 'Free Draw Mode', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, current_color, 4, cv2.LINE_AA)
 
-    if current_mode == SHAPE_MODE:
+    if current_mode == constants.SHAPE_MODE:
         cv2.putText(image, 'Shape Mode', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, current_color, 4, cv2.LINE_AA)
 
-    if current_mode == COLOR_MODE:
+    if current_mode == constants.COLOR_MODE:
         cv2.putText(image, 'Color Mode', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, current_color, 4, cv2.LINE_AA)
-        cv2.putText(image, 'Press r for red', (20, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, RED, 2, cv2.LINE_AA)
-        cv2.putText(image, 'Press b for blue', (20, 160), cv2.FONT_HERSHEY_SIMPLEX, 1, BLUE, 2, cv2.LINE_AA)
-        cv2.putText(image, 'Press g for green', (20, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, GREEN, 2, cv2.LINE_AA)
-        cv2.putText(image, 'Press y for yellow', (20, 240), cv2.FONT_HERSHEY_SIMPLEX, 1, YELLOW, 2, cv2.LINE_AA)
-        cv2.putText(image, 'Press p for purple', (20, 280), cv2.FONT_HERSHEY_SIMPLEX, 1, PURPLE, 2, cv2.LINE_AA)
+        cv2.putText(image, 'Press r for red', (20, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.RED, 2, cv2.LINE_AA)
+        cv2.putText(image, 'Press b for blue', (20, 160), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.BLUE, 2, cv2.LINE_AA)
+        cv2.putText(image, 'Press g for green', (20, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.GREEN, 2, cv2.LINE_AA)
+        cv2.putText(image, 'Press y for yellow', (20, 240), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.YELLOW, 2, cv2.LINE_AA)
+        cv2.putText(image, 'Press p for purple', (20, 280), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.PURPLE, 2, cv2.LINE_AA)
 
-    if current_mode == FREE_DRAW_MODE or current_mode == SHAPE_MODE:
+    if current_mode == constants.FREE_DRAW_MODE or current_mode == constants.SHAPE_MODE:
         cv2.putText(image, 'Press C to change color', (20, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, current_color, 2, cv2.LINE_AA)
 
-    if current_mode == MODE_SWITCHER:
+    if current_mode == constants.MODE_SWITCHER:
         cv2.putText(image, 'Mode Switcher', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, current_color, 4, cv2.LINE_AA)
         cv2.putText(image, 'Press F for free draw', (20, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, current_color, 2, cv2.LINE_AA)
         cv2.putText(image, 'Press S for shape drawing', (20, 160), cv2.FONT_HERSHEY_SIMPLEX, 1, current_color, 2, cv2.LINE_AA)
         cv2.putText(image, "Press V for view only", (20, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, current_color, 2, cv2.LINE_AA)
 
-    cv2.putText(image, 'Press Space to Switch Mode', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, GREEN, 2, cv2.LINE_AA)
+    cv2.putText(image, 'Press Space to Switch Mode', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, constants.GREEN, 2, cv2.LINE_AA)
 
     image = cv2.resize(image, (width, height))
     cv2.imshow("Camera", image)
